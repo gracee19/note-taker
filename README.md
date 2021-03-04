6aa0d2f4c6b6b3af171909e3238f1e2b41a71c8e
Grace Palenzuela <gracee_palenzuela@yahoo.com>
2021-03-04 20:14:24 +1030

    readme,heroku added

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# note-taker
+![Licence](https://img.shields.io/badge/MIT-skyblue)
+# Note Taker
+# Description
+This Project is made so the user will be able to write and save notes. 
+
+To organize the users thoughts and keep track of the task that is needed to be done. 
+
+In this project, the user can write the note and save it and then he/she can view the note at the left hand side of the page and has the option to delete the notes if he/she finish the task or dont want that note anymore.
+
+This is also some of the photo of the deployed project.
+![mainscreen](./public/assets/images/main.png)
+![with-note](./public/assets/images/note.png)
+![delete-note](./public/assets/images/delete.png)
+
+The Project can be also be viewed ![here]().
+
+# Table Of Contents
+Please click on the selection if you want to see a specific part of this README.
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+# Installation
+npm install
+# Usage
+node index.js
+# Licence 
+MIT
+# Contributors
+Grace Palenzuela
+# Test 
+none
+# Questions 
+<br>
+For more information you can check my Github account gracee19(https://github.com/gracee19).<br>
--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3005;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+app.use(express.static('public'));
 
 const notes = [];
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public','notes.html')));
 
 app.get('/api/notes',(req, res) => {
     fs.readFile('./db/db.json', function read(err, data) {
@@ -19,9 +22,52 @@ app.get('/api/notes',(req, res) => {
         }
         var content = JSON.parse(data);
         res.json(content);
-        console.log(content);
+        // console.log(content);
     })
 });
 
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, './index.html')));
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    newNote.id = uuidv4();
+    console.log(newNote);
+    fs.readFile(path.join(__dirname, 'db','db.json'), (err,data) => {
+        if (err) throw err;
+
+        var savedNotes = JSON.parse(data);
+        savedNotes.push(newNote);
+        fs.writeFile(path.join(__dirname, 'db','db.json'),JSON.stringify(savedNotes),(err) => {
+            if (err) throw err;
+            res.json(savedNotes);
+            console.log(savedNotes);
+        });
+
+    });
+});
+
+app.get('/api/notes/:id', (req,res) => {
+    res.json(req.params.id);
+});
+
+app.get('/notes', function(req,res) {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.delete('/api/notes/:id', function(req,res) {
+    fs.readFile('./db/db.json', function (err, data) {
+        if (err) { throw err;}
+        const noteList = JSON.parse(data);
+
+        const newSetNote = noteList.filter(note => note.id !== req.params.id);
+        console.log(newSetNote);
+
+        fs.writeFile(path.join(__dirname, 'db','db.json'),JSON.stringify(newSetNote),(err) => {
+            if (err) throw err;
+            res.json(newSetNote);
+        })
+    });
+    console.log(req.params.id);
+    // res.end();
+});
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public','index.html')));
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
